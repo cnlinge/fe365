18861e535bb2ecef46ad5f5b1647ff6a398577a8
liyingjie06 <liyingjie06@meicai.cn>
2020-11-16 19:23:38 +0800

    柯理化

--- js/柯理化.js
+++ js/柯理化.js
@@ -36,3 +36,25 @@ let fn1=function() {
 }
 
 console.log(fn1());
+
+
+
+function fn(a,b,c) {
+  return a + b + c
+}
+
+function curry(fn, args) {
+  let length = fn.length
+  console.log('length', length)
+  let innerArgs = args || []
+   return function() {
+    var newArgs = innerArgs.concat([...arguments])
+    if (newArgs.length < length) {
+      return curry.call(this, fn, newArgs)
+    } else {
+      return fn.apply(this, newArgs)
+    }
+   }
+}
+const add = curry(fn)
+console.log(add(3)(3)(4)) // 6
